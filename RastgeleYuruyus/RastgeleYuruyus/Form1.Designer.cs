d5efaf04d4ec0c90c18c901d20d88c9c548af17e
ZukanTheHan <70758694+ZukanTheHan@users.noreply.github.com>
2021-01-28 21:04:11 +0300

    Monte Carlo Benzetim Teknigi

---
+++ RastgeleYuruyus/RastgeleYuruyus/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RastgeleYuruyus
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rastgele Yürüyüş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(427, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(588, 76);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(446, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Yürüme Modeli :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yönler :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Uzaklık";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RastgeleYuruyus/RastgeleYuruyus/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace RastgeleYuruyus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random r = new Random();
+            int baslangıcx = 0;           // Rastgele olarak 100 sayı aldım. Bunlar yürüyüşümdeki yönleri belirtecek.
+            int baslangıcy = 0;
+            int x1 = 0;
+            int y1 = 0;
+            int sayi;
+            int i;
+            int[] ilerleyis = new int[100];
+            for(i =0; i < 100; i++)
+            {
+                sayi = r.Next(1, 5);
+                ilerleyis[i] = sayi;
+                listBox1.Items.Add(sayi.ToString());
+            }
+
+            // Her bir yön için birer sayı atadım.
+            for(int k = 0; k < 100; k++)
+            {
+                if(Convert.ToInt32(listBox1.Items[k]) == 1)
+                {
+                    listBox2.Items.Add("İleri");
+                }
+                else if(Convert.ToInt32(listBox1.Items[k]) == 2)
+                {
+                    listBox2.Items.Add("Sağa");
+                }
+                else if (Convert.ToInt32(listBox1.Items[k]) == 3)
+                {
+                    listBox2.Items.Add("Sola");
+                }
+                else if (Convert.ToInt32(listBox1.Items[k]) == 4)
+                {
+                    listBox2.Items.Add("Geri");
+                }
+            }
+            // Belirlediğim yönlerin belirlediğim (x1, y1) noktasını nasıl etkileyeceğini yazdım.
+            for (int l = 0; l < 100; l++)
+            {
+                if (Convert.ToInt32(listBox1.Items[l]) == 1)
+                {
+                    y1 = y1 + 1;
+                }
+                else if (Convert.ToInt32(listBox1.Items[l]) == 2)
+                {
+                    x1 = x1 + 1;
+                }
+                else if (Convert.ToInt32(listBox1.Items[l]) == 3)
+                {
+                    x1 = x1 - 1;
+                }
+                else if (Convert.ToInt32(listBox1.Items[l]) == 4)
+                {
+                    y1 = y1 - 1;
+                }
+            }
+
+            // Pisagor yönteminden yararlanarak başlangıç noktası ile yürüyüşümün sonunda belirlenen nokta arasındaki
+            // uzaklığı buldum.
+            double uzaklık = 0;
+            uzaklık = Math.Sqrt((x1 - baslangıcx) * (x1 - baslangıcx) + ((y1 - baslangıcy) * (y1 - baslangıcy)));
+
+            textBox1.Text = uzaklık.ToString();
+
+
+
+
+
+
+        }
+    }
+}
